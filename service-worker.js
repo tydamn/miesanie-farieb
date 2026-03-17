0cbd22a1dfd3b7bd4af391612894a3dd19d37ddf
tydamn <shoxinus@gmail.com>
2026-03-17 15:33:42 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'miesanie-farieb-v1';
+importScripts('./version.js');
+
+const CACHE_NAME = `miesanie-farieb-${self.APP_VERSION}`;
 const URLS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './version.json',
+  './version.js',
   './icon-192.png',
   './icon-512.png'
 ];
@@ -28,7 +30,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
 
-  if (url.pathname.endsWith('/version.json')) {
+  if (url.pathname.endsWith('/version.js')) {
     event.respondWith(fetch(event.request, { cache: 'no-store' }));
     return;
   }
